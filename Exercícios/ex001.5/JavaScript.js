95ef455825c769bb28bf5b858e4110fdad4b1d41
Lucas Gabriel <lucas.oficial.tube@gmail.com>
2023-03-19 18:22:15 -0300

    formatando strings e números

---
+++ Exercícios/ex001.5/JavaScript.js
@@ -0,0 +1,11 @@
+var nome = String(window.prompt("Qual é o seu nome?"))
+
+var n1 = Number(window.prompt("Digite um númeno:"))
+
+var n2 = Number(window.prompt("Digite outro número:"))
+
+soma = (n1) + (n2)
+
+window.alert(`A soma dos valores ${n1} e ${n2} é ingual a: ${soma}`)
+
+document.write(`O seu nome tem ${nome.length} caracteres.`)
