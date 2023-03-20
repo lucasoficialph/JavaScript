5ffd8ffc1220a084f7f1775a6e7e584169da778a
Lucas Gabriel <lucas.oficial.tube@gmail.com>
2023-03-20 20:56:22 -0300

    Convertendo para moeda local

--- Atividades/incremento/incremento.js
+++ Atividades/incremento/incremento.js
@@ -16,5 +16,20 @@ y = 100
 
 res = x >= y ? `${x} é maior!` : `${y} é maior!`
 
+res1 = x >= y
+
+console.log(res1)
 console.log("operador condicional (ternário)")
 console.log(res)
+
+//   formatando Números
+
+var numero = 1543.5
+
+console.log(numero.toFixed(2))
+
+numero.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL'})
+
+//   Convertendo número para Moeda
+
+console.log(numero.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL'})) //   Mudar a sigla do país, muda a moeda.
