9506ac56ceede5a43ae82b5d9d5a7ba05ef31b23
Lucas Gabriel <lucas.oficial.tube@gmail.com>
2023-03-20 13:26:55 -0300

    contatenação

---
+++ Atividades/contatenação/contatenação.js
@@ -0,0 +1,19 @@
+// caltenando number
+
+var n1 = 10
+
+var n2 = 20
+
+juntar = n1 + n2
+
+console.log(juntar)
+
+// caltenando string
+
+var nome = "Lucas"
+
+var sobrenome = "Gabriel"
+
+nomeCompleto = nome + sobrenome
+
+console.log(`${nome} ${sobrenome}`)
---
+++ Atividades/indentidade/indentidade.js
@@ -0,0 +1,13 @@
+// indentidade
+
+5 == 5   //True
+5 == "5"   //True 
+5 === 5   //True
+5 === "5"   //False
+
+res = 15 == 15
+if (res) {
+    console.log("São inguais")
+} else {
+    console.log("Não são inguais")
+}
