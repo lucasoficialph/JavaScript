03f5c052b458a21a527ac67967be743ebeb76e13
Lucas Gabriel <lucas.oficial.tube@gmail.com>
2023-03-20 14:02:36 -0300

    Formatando strings

---
+++ Atividades/formattando String/formatando_string.js
@@ -0,0 +1,17 @@
+//   descobrindo n° de caracteres de uma string
+
+let nome = "Lucas"
+
+let sobrenome = "Gabriel"
+
+console.log(nome.length + sobrenome.length)
+
+//   tipo da variavel
+
+n = "a"
+
+f = 1
+
+console.log(n.repeat(10))
+console.log(typeof f)
+
