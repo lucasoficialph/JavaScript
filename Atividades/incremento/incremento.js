8987cf0aa74d57c01921f1d8882b4d851b4c1a83
Lucas Gabriel <lucas.oficial.tube@gmail.com>
2023-03-20 20:34:55 -0300

    operador condicional (ternário)

--- Atividades/incremento/incremento.js
+++ Atividades/incremento/incremento.js
@@ -8,3 +8,13 @@ n1 *= 2
 n1 /= 2
 
 console.log(n1)
+
+//   Operador condicional (ternário) 
+
+x = 200
+y = 100
+
+res = x >= y ? `${x} é maior!` : `${y} é maior!`
+
+console.log("operador condicional (ternário)")
+console.log(res)
