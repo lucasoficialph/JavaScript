8fdc5936aaa73e207d41e0dce91dbc81974c5f50
Lucas Gabriel <lucas.oficial.tube@gmail.com>
2023-03-21 11:38:50 -0300

    organizando numeros sort()

--- Atividades/incremento/incremento.js
+++ Atividades/incremento/incremento.js
@@ -28,8 +28,15 @@ var numero = 1543.5
 
 console.log(numero.toFixed(2))
 
-numero.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL'})
+numero.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
 
 //   Convertendo número para Moeda
 
-console.log(numero.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL'})) //   Mudar a sigla do país, muda a moeda.
+console.log(numero.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })) //   Mudar a sigla do país, muda a moeda.
+
+//   Organizando Numeros
+
+var nomes = [1, 3, 2, 5, 4, 7, 6, 8, 9,]; //   se houver dois digitos, o valor retornado, mas é incorrento.
+nomes.sort()
+console.log(nomes);
+
