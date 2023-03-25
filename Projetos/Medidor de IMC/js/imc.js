169748480a12433a49e17bdc49967464ef79eb8d
Lucas Gabriel <lucas.oficial.tube@gmail.com>
2023-03-24 23:01:11 -0300

    Update imc.js

--- Projetos/Medidor de IMC/js/imc.js
+++ Projetos/Medidor de IMC/js/imc.js
@@ -1,12 +1,9 @@
 function calcularImc() {
-let caminhoPeso = window.document.querySelector("#peso")
-let caminhoAltura = window.document.querySelector("#altura")
-let peso = parseFloat(caminhoPeso.value)
-let altura = parseFloat(caminhoAltura.value)**2
-
-let resultado = (peso / altura).toFixed(1)
-
-let meuImc = window.document.querySelector("#meuImc")
-meuImc.innerText = resultado
-
-console.log(resultado)}
+    let caminhoPeso = window.document.querySelector("#peso")
+    let caminhoAltura = window.document.querySelector("#altura")
+    let peso = parseFloat(caminhoPeso.value)
+    let altura = parseFloat(caminhoAltura.value)
+    let resultado = (peso / altura**2).toFixed(1)
+    let meuImc = window.document.querySelector("#meuImc")
+    meuImc.innerText = resultado
+}
