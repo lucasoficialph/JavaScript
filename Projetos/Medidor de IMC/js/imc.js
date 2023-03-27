9a7456d364ba1ba470a9581998af41945716ff8b
Lucas Gabriel <lucas.oficial.tube@gmail.com>
2023-03-27 13:38:15 -0300

    versão 1.5

--- Projetos/Medidor de IMC/js/imc.js
+++ Projetos/Medidor de IMC/js/imc.js
@@ -1,11 +1,65 @@
+
+
 function calcularImc() {
     let caminhoPeso = window.document.querySelector("#peso")
     let caminhoAltura = window.document.querySelector("#altura")
-    let peso = parseFloat(caminhoPeso.value.replace(/^(\d{2})(\d{1})/, "$1.$2"))
-    let altura = parseFloat(caminhoAltura.value.replace(/^(\d{1})(\d{2})/, "$1.$2"))
-    let resultado = (peso / altura**2).toFixed(1)
     let meuImc = window.document.querySelector("#meuImc")
+
+    let peso = parseFloat(caminhoPeso.value)   
+    let altura = parseFloat(caminhoAltura.value)    
+    let resultado = (peso / altura**2).toFixed(1)
     meuImc.innerText = resultado
+
+    let linha1 = document.querySelector("#linha1")
+    let linha2 = document.querySelector("#linha2")
+    let linha3 = document.querySelector("#linha3")
+    let linha4 = document.querySelector("#linha4")
+    let linha5 = document.querySelector("#linha5")
+    let linha6 = document.querySelector("#linha6")
+
+    if(resultado <= 18.5){
+        linha1.style = "background-color: lightblue"
+        linha2.style = "background-color: transparent"
+        linha3.style = "background-color: transparent"
+        linha4.style = "background-color: transparent"
+        linha5.style = "background-color: transparent"
+        linha6.style = "background-color: transparent"    
+    }else if(resultado > 18.5 && resultado < 24.9){ 
+        linha1.style = "background-color: transparent"
+        linha2.style = "background-color: lightblue"
+        linha3.style = "background-color: transparent"
+        linha4.style = "background-color: transparent"
+        linha5.style = "background-color: transparent"
+        linha6.style = "background-color: transparent"
+    }else if(resultado > 25.0 && resultado < 29.9){
+        linha1.style = "background-color: transparent"
+        linha2.style = "background-color: transparent"
+        linha3.style = "background-color: lightblue"
+        linha4.style = "background-color: transparent"
+        linha5.style = "background-color: transparent"
+        linha6.style = "background-color: transparent"
+    }else if(resultado > 30.0 && resultado < 34.9){
+        linha1.style = "background-color: transparent"
+        linha2.style = "background-color: transparent"
+        linha3.style = "background-color: transparent"
+        linha4.style = "background-color: lightblue"
+        linha5.style = "background-color: transparent"
+        linha6.style = "background-color: transparent"
+    }else if(resultado > 35.0 && resultado < 39.9){
+        linha1.style = "background-color: transparent"
+        linha2.style = "background-color: transparent"
+        linha3.style = "background-color: transparent"
+        linha4.style = "background-color: transparent"
+        linha5.style = "background-color: lightblue"
+        linha6.style = "background-color: transparent"
+    }else if(resultado >= 40.0){
+        linha1.style = "background-color: transparent"
+        linha2.style = "background-color: transparent"
+        linha3.style = "background-color: transparent"
+        linha4.style = "background-color: transparent"
+        linha5.style = "background-color: transparent"
+        linha6.style = "background-color: lightblue"
+    }
 }
 
 function mascaraPeso(){
@@ -17,3 +71,4 @@ function mascaraAltura(){
     let altura = document.querySelector("#altura")
     if (altura.value.length == 1){altura.value += "."}
 }
+
