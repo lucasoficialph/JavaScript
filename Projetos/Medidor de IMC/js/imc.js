adc3890f28443513e1364d89ef0256de7365a8f7
Lucas Gabriel <lucas.oficial.tube@gmail.com>
2023-03-26 21:17:19 -0300

    adicionado RegEx na filtragem de dados

--- Projetos/Medidor de IMC/js/imc.js
+++ Projetos/Medidor de IMC/js/imc.js
@@ -1,8 +1,8 @@
 function calcularImc() {
     let caminhoPeso = window.document.querySelector("#peso")
     let caminhoAltura = window.document.querySelector("#altura")
-    let peso = parseFloat(caminhoPeso.value.replace(",","."))
-    let altura = parseFloat(caminhoAltura.value.replace(",","."))
+    let peso = parseFloat(caminhoPeso.value.replace(/^(\d{2})(\d{1})/, "$1.$2"))
+    let altura = parseFloat(caminhoAltura.value.replace(/^(\d{1})(\d{2})/, "$1.$2"))
     let resultado = (peso / altura**2).toFixed(1)
     let meuImc = window.document.querySelector("#meuImc")
     meuImc.innerText = resultado
