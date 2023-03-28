87703d490583d50ec7eb84f288aca9f827e365b7
Lucas Gabriel <lucas.oficial.tube@gmail.com>
2023-03-28 13:13:32 -0300

    remove exercicio

---
+++ Atividades/while/while.js
@@ -0,0 +1,17 @@
+var x = 20
+
+while (x < 200){
+    x++
+    if(x == 100){
+        console.log("Parou")
+        break
+    }
+    console.log(x)
+}
+var y = 200
+
+while (y > 0){
+    y--
+    console.log(y)
+}
+
--- Exercícios/ex001.5/JavaScript.js
+++
@@ -1,7 +0,0 @@
-var nome = "Lucas"
-var idade = 20
-var altura = 1.80
-
-window.alert(`O aluno ${nome} tem ${idade} anos e ${altura} de altura.`)
-
-
