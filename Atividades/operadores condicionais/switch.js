dcd9fac20ffdbbba5b2923768e619802cc9a6480
Lucas Gabriel <lucas.oficial.tube@gmail.com>
2023-03-31 09:34:40 -0300

    Exercício switch e Boolean

---
+++ Atividades/operadores condicionais/switch.js
@@ -0,0 +1,22 @@
+let nome = "Lucas"
+
+switch(nome){
+    case "Gabriel":
+        console.log("6 - Gabriel")
+        break
+    case "Lucas":
+        console.log("5 - Lucas")
+        break
+    default:
+    console.log("[ERRO] Nome não encontrado!")
+}
+
+// Testando valores Booleans
+
+var valor1 = true
+var valor2 = false
+
+// convertendo valores Booleans
+
+var valor2 = Boolean(true)
+console.log(valor1, valor2)
