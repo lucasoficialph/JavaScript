77d703ee5a5e7bfa56d88b412dccc413c10a8ddc
Lucas Gabriel <lucas.oficial.tube@gmail.com>
2023-04-04 07:25:22 -0300

    menu com js

---
+++ Projetos/Verificador de idade/js/script.js
@@ -0,0 +1,9 @@
+function resultado(){
+    let dataNacimento = document.querySelector("#dataNacimento").value
+    let sexoMasculino = document.querySelector("#sexoMasculino").value
+    let sexoFeminino = document.querySelector("#sexoFeminino").value
+
+
+    console.log(sexoFeminino.typeof)
+}
+
---
+++ test/test.js
@@ -0,0 +1,8 @@
+function resultado(){
+    let dataNacimento = document.querySelector("#dataNacimento").value
+    let sexoMasculino = document.querySelector("#masculino").value
+    let sexoFeminino = document.querySelector("#feminino").value
+
+    console.log(dataNacimento, sexoFeminino, sexoMasculino)
+}
+
