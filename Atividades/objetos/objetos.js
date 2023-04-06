0a2dc19bc847ac81e00746fe7aef1b353e722dd4
Lucas Gabriel <lucas.oficial.tube@gmail.com>
2023-04-05 21:22:59 -0300

    Create objetos.js

---
+++ Atividades/objetos/objetos.js
@@ -0,0 +1,18 @@
+let obj = {
+    nome: "Lucas",
+    sobreNome: "Gabriel",
+    idade: 20,
+    peso: 72.5
+}
+
+console.log(typeof obj)
+console.log(obj)
+console.log(obj.nome)
+console.log(obj.sobreNome)
+console.log(obj.idade)
+console.log(obj.peso)
+
+console.log(typeof obj.nome)
+console.log(typeof obj.sobreNome)
+console.log(typeof obj.idade)
+console.log(typeof obj.peso)
