3e19fc6461e35a72edfdd16a015b00f1af33d3d4
Lucas Gabriel <lucas.oficial.tube@gmail.com>
2023-04-10 12:07:24 -0300

    media screen

--- test/test.js
+++
@@ -1,8 +0,0 @@
-function resultado(){
-    let dataNacimento = document.querySelector("#dataNacimento").value
-    let sexoMasculino = document.querySelector("#masculino").value
-    let sexoFeminino = document.querySelector("#feminino").value
-
-    console.log(dataNacimento, sexoFeminino, sexoMasculino)
-}
-
