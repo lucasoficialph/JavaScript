e5d421d03fed778537863559fb9834670a25dd9a
Lucas Gabriel <lucas.oficial.tube@gmail.com>
2023-04-18 12:25:52 -0300

    js

---
+++ Atividades/for/loops.js
@@ -0,0 +1,13 @@
+var nome = 'lucas'
+
+for (var i = 1; i <= nome.length ; i++){
+    console.log(i)
+}
+
+console.log('--------------------------')
+
+let array = ['Lucas', 'Danyel', 'Mônica']
+
+for(var i = 0; i < array.length; i++){
+    console.log(i)
+}
---
+++ Atividades/for/loops1.js
@@ -0,0 +1,11 @@
+var frase = 'Vivendo uma vida linda sem preocupações.'
+
+var array = ['Lucas', 'Danyel', 'Mônica']
+
+for(var i of array){
+    console.log(i)
+}
+
+array[array.length] = 'Mateus'
+
+console.log(array)
